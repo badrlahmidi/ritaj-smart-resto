2819bb89209d781cef62f509a493a7f6af5b449b
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 20:59:40 +0100

    Fix key too long error by setting default string length to 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Order;
 use App\Observers\OrderObserver;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Order::observe(OrderObserver::class);
     }
 }
