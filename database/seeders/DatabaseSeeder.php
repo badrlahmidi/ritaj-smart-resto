4e8b82092d623dd56d3ff92421f59170dc4062e2
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 21:09:16 +0100

    Ensure admin user is created in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,31 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Création User Admin (si n'existe pas déjà)
+        $user = \App\Models\User::firstOrCreate(
+            ['email' => 'admin@ritaj.com'], 
+            [
+                'name' => 'Admin Ritaj',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Autres seeders (optionnel)
+        // $this->call([
+        //     ProductSeeder::class,
+        // ]);
     }
 }
