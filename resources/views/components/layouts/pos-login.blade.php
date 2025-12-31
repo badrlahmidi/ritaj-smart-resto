e31d2cc9dbc3df1b40155413f9e80d540e0a68ac
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 18:44:28 +0100

    Create minimal POS Login layout

---
+++ resources/views/components/layouts/pos-login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-900">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>POS Login - Ritaj Smart Resto</title>
+    @filamentStyles
+    @vite('resources/css/app.css')
+</head>
+<body class="h-full">
+    {{ $slot }}
+    @filamentScripts
+    @vite('resources/js/app.js')
+</body>
+</html>
