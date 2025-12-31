cd487613d25710523fff507d2d81b4a0aec6a6c5
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 18:45:08 +0100

    Register POS Login Route in web.php (correct SHA)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Livewire\CashRegisterLogic;
+use App\Livewire\Pos\PosLoginPage;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
+
+// POS Routes
+Route::get('/pos/login', PosLoginPage::class)->name('pos.login');
+
+// Protected POS Routes (To be grouped later)
+// Route::get('/pos', PosPage::class)->middleware('auth');
