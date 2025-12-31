37323a77d5f59f94d32b5006880a75ab952d6731
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 22:23:26 +0100

    Add tailwind config to support POS layout

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import preset from './vendor/filament/support/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+}
