dd06559eb3a5d24900eab6fb04165e5daa3624d6
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 18:56:15 +0100

    Update CreateUser page to handle PIN creation (correct SHA)

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,10 +3,24 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\UserPin;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function afterCreate(): void
+    {
+        $data = $this->form->getRawState();
+        
+        if (!empty($data['new_pin'])) {
+            UserPin::create([
+                'user_id' => $this->record->id,
+                'pin_code' => Hash::make($data['new_pin']),
+            ]);
+        }
+    }
 }
