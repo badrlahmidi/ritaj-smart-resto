09007886f01777099a3f5454e6239382a330013d
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 19:30:51 +0100

    Register KDS routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\CashRegisterLogic;
 use App\Livewire\Pos\PosLoginPage;
 use App\Livewire\Pos\PosPage;
 use App\Livewire\Pos\PosOrderPage;
+use App\Livewire\Kds\KdsBoard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +26,7 @@ Route::get('/', function () {
 Route::get('/pos/login', PosLoginPage::class)->name('pos.login');
 Route::get('/pos', PosPage::class)->middleware('auth')->name('pos');
 Route::get('/pos/order/{table}', PosOrderPage::class)->middleware('auth')->name('pos.order');
+
+// KDS Routes
+Route::get('/kds', KdsBoard::class)->middleware('auth')->name('kds'); // Master KDS
+Route::get('/kds/{station}', KdsBoard::class)->middleware('auth')->name('kds.station'); // Station specific
