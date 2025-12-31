086ebfb55f0e89d5c14ffda256cfaf124712bcc7
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 19:45:08 +0100

    Register Payment Route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\CashRegisterLogic;
 use App\Livewire\Pos\PosLoginPage;
 use App\Livewire\Pos\PosPage;
 use App\Livewire\Pos\PosOrderPage;
+use App\Livewire\Pos\PosPaymentPage;
 use App\Livewire\Kds\KdsBoard;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,7 @@ Route::get('/', function () {
 Route::get('/pos/login', PosLoginPage::class)->name('pos.login');
 Route::get('/pos', PosPage::class)->middleware('auth')->name('pos');
 Route::get('/pos/order/{table}', PosOrderPage::class)->middleware('auth')->name('pos.order');
+Route::get('/pos/payment/{order}', PosPaymentPage::class)->middleware('auth')->name('pos.payment');
 
 // KDS Routes
 Route::get('/kds', KdsBoard::class)->middleware('auth')->name('kds'); // Master KDS
