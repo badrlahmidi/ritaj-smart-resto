bb030c6f61938c3fd67b7078ff14809bee042139
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 19:44:25 +0100

    Create Payment Model

---
+++ app/Models/Payment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'amount',
+        'payment_method',
+        'amount_tendered',
+        'change_due',
+        'transaction_reference',
+        'user_id',
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function cashier(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
