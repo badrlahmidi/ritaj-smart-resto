886aa8400e7aa257c4aed680ebfced7d74f986b9
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 20:15:45 +0100

    Create ListUsers Page

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -9,11 +9,5 @@ use Filament\Resources\Pages\ListRecords;
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
+    protected function getHeaderActions(): array { return [Actions\CreateAction::make()]; }
 }
