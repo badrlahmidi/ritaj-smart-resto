86b9126489c5f2f2521b627b6b9de9a3bf5c8b4f
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 22:20:46 +0100

    Update POS blade wrapper to remove default Filament padding

--- resources/views/filament/pages/waiter-pos.blade.php
+++ resources/views/filament/pages/waiter-pos.blade.php
@@ -1,3 +1,3 @@
-<x-filament-panels::page>
+<div class="filament-page-simple-layout w-full h-screen bg-gray-50 dark:bg-gray-900 overflow-hidden m-0 p-0">
     @livewire('pos-interface')
-</x-filament-panels::page>
+</div>
