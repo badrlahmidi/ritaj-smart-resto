e868fe8a77e6a221135b06e01fa27b56707baa74
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 18:56:27 +0100

    Update EditUser page to handle PIN updates (correct SHA)

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\UserPin;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditUser extends EditRecord
 {
@@ -15,5 +17,17 @@ class EditUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $data = $this->form->getRawState();
+        
+        if (!empty($data['new_pin'])) {
+            UserPin::updateOrCreate(
+                ['user_id' => $this->record->id],
+                ['pin_code' => Hash::make($data['new_pin'])]
+            );
+        }
     }
 }
