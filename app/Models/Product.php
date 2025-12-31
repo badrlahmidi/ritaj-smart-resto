df06d95d8e354560644b9e5fcde59f1fd79a9bff
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 17:33:10 +0100

    Update Product model to include relationship with Ingredient

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -32,5 +33,12 @@ class Product extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class)
+            ->withPivot(['quantity', 'unit'])
+            ->withTimestamps();
     }
 }
