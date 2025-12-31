f05deafd84aae8161194072bdfc413e881c19aa4
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 18:45:39 +0100

    Create POS App Layout

---
+++ resources/views/components/layouts/pos-app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <title>POS - Ritaj Smart Resto</title>
+    @filamentStyles
+    @vite('resources/css/app.css')
+    <style>
+        /* Hide scrollbar for Chrome, Safari and Opera */
+        .no-scrollbar::-webkit-scrollbar {
+            display: none;
+        }
+        /* Hide scrollbar for IE, Edge and Firefox */
+        .no-scrollbar {
+            -ms-overflow-style: none;  /* IE and Edge */
+            scrollbar-width: none;  /* Firefox */
+        }
+    </style>
+</head>
+<body class="h-full overflow-hidden">
+    {{ $slot }}
+    
+    @filamentScripts
+    @vite('resources/js/app.js')
+</body>
+</html>
