8773726d4f356cf875a7838a025a6ab86efe66a8
badrlahmidi <78702514+badrlahmidi@users.noreply.github.com>
2025-12-31 15:31:26 +0100

    Resolve merge conflict markers in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,18 +10,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-=======
-use Illuminate\Support\Facades\Schema; // <--- AJOUTER CETTE LIGNE
-
-class AppServiceProvider extends ServiceProvider
-{
->>>>>>> 80726fd (Fix local migration issues manually)
     public function register(): void
     {
         //
     }
 
-<<<<<<< HEAD
     /**
      * Bootstrap any application services.
      */
@@ -30,10 +22,5 @@ class AppServiceProvider extends ServiceProvider
     {
         // Fix pour l'erreur "Key too long" sur les anciennes versions de MySQL/MariaDB
         Schema::defaultStringLength(191);
-=======
-    public function boot(): void
-    {
-        Schema::defaultStringLength(191); // <--- AJOUTER CETTE LIGNE
->>>>>>> 80726fd (Fix local migration issues manually)
     }
 }
